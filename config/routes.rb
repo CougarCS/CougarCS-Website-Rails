324b71451f65b0063028a209e46d4f9f925eeb5d
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-09-09 22:52:07 -0500

    added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 CougarCSWebsite::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/about"
+  get "static_pages/contact"
   
   match '/about', to: 'static_pages#about', via: [:get]
+  match '/contact', to: 'static_pages#contact', via: [:get]
   
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
