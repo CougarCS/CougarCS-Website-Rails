4e104f93f2723285436adced97514a5c2ce36c81
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-09-10 22:20:45 -0500

    adding secret_token file again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CougarCSWebsite::Application.config.secret_key_base = '45cf18f540fb0782c20069878b1e5fac772722888a1bbd539cf90efd5ffe32637642b466abead5854024cda64e2fc6f69707fa5bc6a40f70df8b749685d22df1'
