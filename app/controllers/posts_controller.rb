2db37650b95db8489f59427167f80b6a4e5f2ec4
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-09-28 21:51:41 -0500

    post controller requires author login now and modified styling of couple of devise generated pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_author!
 
   # GET /posts
   # GET /posts.json
