8a69be60238932ca3f4b4b1ce5982721b92be824
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-09-30 23:48:14 -0500

    authenticating author in author controller, changed splash image background color

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_author!
 
   # GET /authors
   # GET /authors.json
