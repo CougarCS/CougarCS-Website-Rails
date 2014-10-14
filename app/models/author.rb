cb7202c442c12fc1e7cd8a1737778992caf5f8e6
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-10-14 17:46:20 -0500

    added friendly id slug to authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,11 +4,18 @@ class Author < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   # :registerable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   
   has_attached_file :avatar, :default_url => "/images/default.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+  
+  extend FriendlyId
+  friendly_id :full_name, use: [:slugged, :finders]
+  
+  def full_name
+    "#{first_name} #{last_name}"
+  end
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014215906) do
+ActiveRecord::Schema.define(version: 20141014223438) do
 
   create_table "authors", force: true do |t|
     t.string   "first_name"
@@ -33,11 +33,13 @@ ActiveRecord::Schema.define(version: 20141014215906) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "slug"
   end
 
   add_index "authors", ["email"], name: "index_authors_on_email", unique: true
   add_index "authors", ["posts_id"], name: "index_authors_on_posts_id"
   add_index "authors", ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
+  add_index "authors", ["slug"], name: "index_authors_on_slug"
 
   create_table "posts", force: true do |t|
     t.string   "title"
