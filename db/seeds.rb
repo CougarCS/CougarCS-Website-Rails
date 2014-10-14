bf2f850b238239bd203c2b9c18ccdc0fb76bc2ed
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-10-14 16:21:52 -0500

    html_safe post content

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Post.create(title: '1st CougarCS Meeting For Fall 2014', body: 'First official Fall 2014 CougarCS meeting will be on Thursday, September 11 at 05:30 PM. We’ll go over all the new exciting things that’ll happen this year (like career fair, tailgate, etc.). We also have Dr. Hesam Panahi from Red Labs coming, who is  in charge of the startup incubator (accelerator) over at the UH Red Labs. In addition to that, Jared Bienz from Microsoft will be giving a talk on virtual reality, and will demo how to use the Oculus SDK in your Unity game!', date: '2014-09-09', author_id: 3)
+Post.create(title: '1st CougarCS Meeting For Fall 2014', body: '<p>
+					First official Fall 2014 CougarCS meeting will be on Thursday, September 11 at 05:30 PM. We’ll go over all the new exciting things that’ll happen this year (like career fair, tailgate, etc.). We also have Dr. Hesam Panahi from Red Labs coming, who is  in charge of the startup incubator (accelerator) over at the UH Red Labs. In addition to that, Jared Bienz from Microsoft will be giving a talk on virtual reality, and will demo how to use the Oculus SDK in your Unity game!
+				</p>
+				<p>
+					So <a href="https://www.facebook.com/events/1473345866261115/1473686369560398/">RSVP</a> to the meeting and come enjoy some FREE food and socialize with your fellow comrades!
+				</p>', date: '2014-09-09', author_id: 3)
 
 Post.create(title: 'Houston Techfest', body: 'Houston Techfest is this Saturday September 13 at Reliant Center. Free to attend, over 1,500 attendees, with multiple tracks ranging from .NET, JavaScript, Mobile Development, Cloud, Agile Methods. Great opportunities to network and learn from professionals in the Houston area! Come see what the Houston Developer scene is all about!', date: '2014-09-08', author_id: 3)
 
