60d2b3f26ac2ff5059caff83caae10e1662ab024
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-10-14 17:49:02 -0500

    author show page is public now

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_author!
+  before_action :authenticate_author!, except: [:show]
 
   # GET /authors
   # GET /authors.json
