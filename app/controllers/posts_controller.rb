7c29dc355eafb6549721e999d17044e39de62771
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-10-14 18:36:30 -0500

    added delete button to delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -55,7 +55,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url }
+      format.html { redirect_to root_url, notice: 'Post was successfully deleted.' }
       format.json { head :no_content }
     end
   end
