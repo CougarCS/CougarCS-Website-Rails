6c0b8d076ad529660632cefed84444dc9ec34848
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-10-14 17:33:08 -0500

    made show post look pretty

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_author!
+  before_action :authenticate_author!, except: [:show]
 
   # GET /posts
   # GET /posts.json
